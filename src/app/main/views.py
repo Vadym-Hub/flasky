c3956ab109e46fc8c9ec90471a396941dbb81b13
Vadim <vadim.is.pashynskyi@gmail>
2020-10-07 19:41:47 +0300

    Add unit tests with Selenium

--- src/app/main/views.py
+++ src/app/main/views.py
@@ -9,6 +9,18 @@ from ..models import Permission, Role, User, Post, Comment
 from ..decorators import admin_required, permission_required
 
 
+@main.route('/shutdown')
+def server_shutdown():
+    """Отключает сервер Flask через HTTP-запрос(для тестировки)"""
+    if not current_app.testing:
+        abort(404)
+    shutdown = request.environ.get('werkzeug.server.shutdown')
+    if not shutdown:
+        abort(500)
+    shutdown()
+    return 'Shutting down...'
+
+
 @main.route('/', methods=['GET', 'POST'])
 def index():
     form = PostForm()
