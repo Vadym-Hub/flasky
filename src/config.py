952fba4235a9be4c74a789f3a50852d7578c5e2b
Vadim <vadim.is.pashynskyi@gmail>
2020-10-08 18:21:58 +0300

    Deploy to heroku

--- src/config.py
+++ src/config.py
@@ -13,6 +13,7 @@ class Config:
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    SSL_REDIRECT = False  # Для пакета flask-sslify
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASKY_POSTS_PER_PAGE = 20  # Пагинация страницы постов
     FLASKY_FOLLOWERS_PER_PAGE = 50  # Пагинация страницы подписчиков
@@ -71,10 +72,31 @@ class ProductionConfig(Config):
         app.logger.addHandler(mail_handler)
 
 
+class HerokuConfig(ProductionConfig):
+    """Конфигурация для Heroku."""
+    SSL_REDIRECT = True if os.environ.get('DYNO') else False  # Для пакета flask-sslify
+
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+
+        # handle reverse proxy server headers
+        from werkzeug.contrib.fixers import ProxyFix
+        app.wsgi_app = ProxyFix(app.wsgi_app)
+
+        # log to stderr
+        import logging
+        from logging import StreamHandler
+        file_handler = StreamHandler()
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+
 config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
     'production': ProductionConfig,
+    'heroku': HerokuConfig,
 
     'default': DevelopmentConfig
 }
