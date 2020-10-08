c2ffeafc2f12fa23f11e73fc3e723025dcc7da6a
Vadim <vadim.is.pashynskyi@gmail>
2020-10-08 19:00:41 +0300

    For traditional hosting

--- src/flasky.py
+++ src/flasky.py
@@ -1,6 +1,11 @@
 import os
+from dotenv import load_dotenv
 
-COV = None  #
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+COV = None
 if os.environ.get('FLASK_COVERAGE'):
     import coverage
     COV = coverage.coverage(branch=True, include='app/*')
